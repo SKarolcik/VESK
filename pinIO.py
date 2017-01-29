242b0ea1e5674ee77fa951230b42291701e53294
Steve <s.karolcik@gmail.com>
2017-01-29 03:33:10 +0000

    Updated source

--- pinIO.py
+++ pinIO.py
@@ -1,9 +1,32 @@
 from wiringx86 import GPIOEdison as GPIO
+import random
+
 gpio = GPIO(debug=False)
 gpio1 =GPIO(debug=False)
 # Set up an LED on pin 6
-gpio.pinMode(10, gpio.OUTPUT)
-gpio.digitalWrite(10, gpio.HIGH)
+# Prepare pins and loop
 
+gpio.pinMode(10, gpio.OUTPUT)
 gpio1.pinMode(11, gpio1.OUTPUT)
-gpio1.digitalWrite(11, gpio1.HIGH)
+
+try:
+    while(True):
+        # Write a state to the pin. ON or OFF.
+        val = random.randint(0,255)
+        val1 = random.randint(0,255)
+        gpio.analogWrite(10,val)
+        gpio1.analogWrite(11,val)
+
+        # Sleep for a while.
+        time.sleep(1)
+
+# When you get tired of seeing the led blinking kill the loop with Ctrl-C.
+except KeyboardInterrupt:
+    # Leave the led turned off.
+    print '\nCleaning up...'
+    gpio.digitalWrite(10, gpio.LOW)
+    gpio1.digitalWrite(11, gpio1.LOW)
+
+    # Do a general cleanup. Calling this function is not mandatory.
+    gpio.cleanup()
+    gpio1.cleanup()
