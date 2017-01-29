b0e301bd345230b651cbdc76a851edfa04f0b1ac
Steve <s.karolcik@gmail.com>
2017-01-29 06:08:38 +0000

    Final version, hopefully

---
+++ Edison_final.py
@@ -0,0 +1,71 @@
+import paho.mqtt.client as paho
+from wiringx86 import GPIOEdison as GPIO
+import time
+
+gpio = GPIO(debug=False)
+gpio1 =GPIO(debug=False)
+# Set up an LED on pin 6
+# Prepare pins and loop
+
+gpio.pinMode(9, gpio.PWM)
+gpio1.pinMode(6, gpio1.PWM)
+
+# Define event callbacks
+def process_input (numbers):
+	lis_num = [x.strip() for x in numbers.split(',')]
+	val1 = int((int(lis_num[1])/10000)*255)
+	val2 = int((int(lis_num[2])/10000)*255)
+	gpio.analogWrite(9,val)
+    gpio1.analogWrite(6,val1)
+
+
+def on_connect(mosq, obj, rc):
+    print("rc: " + str(rc))
+
+def on_message(mosq, obj, msg):
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+    process_input(str(msg.payload))
+
+def on_publish(mosq, obj, mid):
+    print("mid: " + str(mid))
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+def on_log(mosq, obj, level, string):
+    print(string)
+
+mqttc = paho.Client()
+# Assign event callbacks
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+mqttc.on_subscribe = on_subscribe
+
+
+# Uncomment to enable debug messages
+#mqttc.on_log = on_log
+
+# Connect
+mqttc.username_pw_set('ccdmmpze',password = '4dE8WJGSbqja')
+mqttc.connect('m20.cloudmqtt.com',18428,2)
+# Start subscribe, with QoS level 0
+
+mqttc.loop_start()
+mqttc.subscribe("sensor/rgb", 1)
+time.sleep(120)
+
+mqttc.disconnect()
+mqttc.loop_stop()
+gpio.pinMode(9, gpio.OUTPUT)
+gpio1.pinMode(6, gpio1.OUTPUT)
+gpio.digitalWrite(9, gpio.LOW)
+gpio1.digitalWrite(6, gpio1.LOW)
+
+    # Do a general cleanup. Calling this function is not mandatory.
+gpio.cleanup()
+gpio1.cleanup()
+
+# Publish a message
+
+
